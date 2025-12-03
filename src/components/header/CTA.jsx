2edf22ce47fdee8c0cc14cde96b65cf621150781
dhiva1312 <dhivakarvasu2000@gmail.com>
2025-12-03 17:33:14 +0530

    Updated Resume

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/DhivakarV.pdf'
+import CV from '../../assets/DhivakarV_Fullstack.pdf'
 const CTA = () => {
   return (
     <div className='cta'>
